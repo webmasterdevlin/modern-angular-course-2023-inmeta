824b74a1124c5ac83296451b1a13b84eff8564ee
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2023-06-24 11:39:55 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterModule],
   template: `
     <div class="container prose mx-auto">
-      <h1>Modern Angular Course</h1>
+      <router-outlet></router-outlet>
     </div>
   `,
 })
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,11 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
+import {
+  provideRouter,
+  withEnabledBlockingInitialNavigation,
+} from '@angular/router';
+import { appRoutes } from './app/app.routes';
 
-bootstrapApplication(AppComponent).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, {
+  providers: [provideRouter(appRoutes, withEnabledBlockingInitialNavigation())],
+}).catch((err) => console.error(err));
