1bdd4bbcb42593d674ba00171577439fa9902ef7
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2023-06-24 12:26:38 +0200

    update

--- src/app/pages/todos.component.ts
+++ src/app/pages/todos.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
+import { Actions, State } from '../store';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
   imports: [SharedModule],
-  template: `<p>todos works!</p>`,
+  template: ``,
   styles: [],
 })
-export class TodosComponent {}
+export class TodosComponent implements OnInit {
+  private _stateService = inject(State);
+  private _actionsService = inject(Actions);
+
+  store = this._stateService.store;
+
+  async ngOnInit() {
+    await this._actionsService.fetchTodos();
+  }
+}
