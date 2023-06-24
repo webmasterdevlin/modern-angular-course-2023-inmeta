c17f2f3f11560c24742198b5892824443b9ac6ac
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2023-06-24 12:02:29 +0200

    update

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -1,9 +1,24 @@
 import { Injectable } from '@angular/core';
+import { EndPointsKeys, api } from '../configs/axios.config';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HttpService {
-
-  constructor() { }
+  async get<T>(endpoint: EndPointsKeys) {
+    return await api.get<T>(`${endpoint}?_start=0&_limit=10`);
+  }
+  async delete<T>(endpoint: EndPointsKeys, id: number) {
+    return await api.delete<T>(`${endpoint}/${id}`);
+  }
+  async post<T>(endpoint: EndPointsKeys, data: any) {
+    return await api.post<T>(`${endpoint}?_start=0&_limit=10`, data);
+  }
+  async put<ResponseT, BodyT>(
+    endpoint: EndPointsKeys,
+    id: number,
+    data: BodyT
+  ) {
+    return await api.put<ResponseT>(`${endpoint}/${id}`, data);
+  }
 }
