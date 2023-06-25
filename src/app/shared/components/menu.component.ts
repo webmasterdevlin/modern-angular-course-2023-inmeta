85361e0644ff14c4434b56775c00f9872322081a
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2023-06-25 10:15:50 +0200

    update

--- src/app/shared/components/menu.component.ts
+++ src/app/shared/components/menu.component.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-menu',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>menu works!</p>`,
-  styleUrls: ['./menu.component.css'],
+  template: `
+    <button
+      (click)="to()"
+      class="mb-2 mr-2 rounded-lg border border-gray-300 bg-white 
+      px-5 py-2.5 text-sm font-medium text-gray-900 
+      hover:bg-gray-100 focus:outline-none focus:ring-4 focus:ring-gray-200"
+    >
+      {{ label }} {{ counter }}
+    </button>
+  `,
 })
-export class MenuComponent {}
+export class MenuComponent {
+  @Input({ required: true })
+  label: string | null = null;
+
+  @Input()
+  counter: number | null = null;
+
+  @Output()
+  handleClick = new EventEmitter<void>();
+
+  to() {
+    this.handleClick.emit();
+  }
+}
