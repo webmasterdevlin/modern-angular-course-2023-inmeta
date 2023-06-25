a75684b38611f356fb1b1ef7e3e1658c128530c5
devlin <73556235+mandaloriandeveloper@users.noreply.github.com>
2023-06-25 10:34:23 +0200

    update

--- src/app/shared/components/footer.component.ts
+++ src/app/shared/components/footer.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Getters, State } from 'src/app/store';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  template: `<p>footer works!</p>`,
+  template: `
+    <footer class="my-5 bg-white">
+      <pre class="font-bold">
+        todos: {{ store().todos.length }} | posts: {{ store().posts.length }}
+      </pre
+      >
+      <pre>Combined posts and todos: {{ total }}</pre>
+    </footer>
+  `,
 })
 export class FooterComponent {
-  
+  stateService = inject(State);
+  store = this.stateService.store;
+  private _gettersService = inject(Getters);
+  total = this._gettersService.totalObjects();
 }
